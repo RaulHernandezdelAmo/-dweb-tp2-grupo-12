622d000d5c6205bb277a40d495c8b5b26e614dce
Raul Hernandez Del Amo <m307624@alunos.ipb.pt>
2022-05-02 19:19:07 +0100

    composer configuration

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '20e7265f417148c3be6d35a342662c46fae7706f',
+        'name' => 'vendor_name/dweb-tp2-grupo-12',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'components/jquery' => array(
+            'pretty_version' => '3.6.0',
+            'version' => '3.6.0.0',
+            'type' => 'component',
+            'install_path' => __DIR__ . '/../components/jquery',
+            'aliases' => array(),
+            'reference' => '6cf38ee1fd04b6adf8e7dda161283aa35be818c3',
+            'dev_requirement' => false,
+        ),
+        'twbs/bootstrap' => array(
+            'pretty_version' => 'v4.6.1',
+            'version' => '4.6.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twbs/bootstrap',
+            'aliases' => array(),
+            'reference' => '043a03c95a2ad6738f85b65e53b9dbdfb03b8d10',
+            'dev_requirement' => false,
+        ),
+        'twitter/bootstrap' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => 'v4.6.1',
+            ),
+        ),
+        'vendor_name/dweb-tp2-grupo-12' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '20e7265f417148c3be6d35a342662c46fae7706f',
+            'dev_requirement' => false,
+        ),
+    ),
+);
